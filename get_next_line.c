02f119929a4534f31ad470857c3a7ac702bdecb3
Isadora Orsini Barradas <iorsini-@c1r5s1.42porto.com>
2025-06-14 16:50:14 +0100

    Jun14

---
+++ get_next_line.c
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iorsini- <iorsini-@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/26 15:28:32 by iorsini-          #+#    #+#             */
+/*   Updated: 2025/06/14 16:39:39 by iorsini-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+//#include <stdio.h>
+//#include <fcntl.h>
+
+char	*get_next_line(int fd)
+{
+	static char	buffer[BUFFER_SIZE + 1];
+	char		*line;
+	int			bytes_read;
+
+	if (fd < 0 || fd > 1024 || BUFFER_SIZE < 1)
+		return (NULL);
+	line = NULL;
+	line = ft_strjoin(line, buffer);
+	while (!ft_strchr(buffer))
+	{
+		bytes_read = read(fd, buffer, BUFFER_SIZE);
+		if (bytes_read == -1 || buffer[0] == 0)
+		{
+			free(line);
+			shift(buffer);
+			return (NULL);
+		}
+		if (!bytes_read)
+			break ;
+		if (bytes_read < BUFFER_SIZE)
+			buffer[bytes_read] = '\0';
+		line = ft_strjoin(line, buffer);
+	}
+	shift(buffer);
+	return (line);
+}
+/*
+int	main(void)
+{
+	int		fd;
+	char	*linha;
+	int		contador = 1;
+
+	fd = open("teste.txt", O_RDONLY);
+	if (fd < 0)
+	{
+		perror("Erro ao abrir o arquivo");
+		return (1);
+	}
+	while ((linha = get_next_line(fd)) != NULL)
+	{
+		printf("Chamada %d: %s", contador, linha);
+		free(linha);
+		contador++;
+	}
+	close(fd);
+	return (0);
+}*/
---
+++ get_next_line.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iorsini- <iorsini-@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/26 15:31:01 by iorsini-          #+#    #+#             */
+/*   Updated: 2025/06/14 16:40:30 by iorsini-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+//  malloc, free, size_t
+# include <stdlib.h>
+
+//  close, read
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+char	*ft_strjoin(char *s1, char *s2);
+int		ft_strlen(char *s);
+int		ft_strchr(const char *s);
+void	shift(char *buffer);
+
+#endif
